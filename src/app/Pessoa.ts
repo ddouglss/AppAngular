0ef77b5046ad75c752a9b56eae9cf4646c143bf4
ddouglss <ddouglss1999@gmail.com>
2024-11-22 22:57:48 -0300

    Inserindo a função do botão Voltar/Atualizar

--- src/app/Pessoa.ts
+++ src/app/Pessoa.ts
@@ -1,35 +1,19 @@
-// Define uma classe chamada 'Pessoa', que representa um modelo de dados para uma pessoa.
-export class Pessoa { 
-    // Declara uma propriedade `pessoaId` do tipo `number`, que provavelmente é o identificador único da pessoa.
-    pessoaId: number; 
 
-    // Declara uma propriedade `nome` do tipo `string`, que armazena o primeiro nome da pessoa.
-    nome: string;
+export class Pessoa { // Define uma classe chamada 'Pessoa', que representa um modelo de dados para uma pessoa.
+    pessoaId: number; // Declara uma propriedade `pessoaId` do tipo `number`, que provavelmente é o identificador único da pessoa.
+    nome: string;// Declara uma propriedade `nome` do tipo `string`, que armazena o primeiro nome da pessoa.
+    sobrenome: string;// Declara uma propriedade `sobrenome` do tipo `string`, que armazena o sobrenome da pessoa.   
+    idade: number;// Declara uma propriedade `idade` do tipo `number`, que armazena a idade da pessoa.
+    profissao: string;// Declara uma propriedade `profissao` do tipo `string`, que armazena a profissão da pessoa.
 
-    // Declara uma propriedade `sobrenome` do tipo `string`, que armazena o sobrenome da pessoa.
-    sobrenome: string;
 
-    // Declara uma propriedade `idade` do tipo `number`, que armazena a idade da pessoa.
-    idade: number;
+    constructor(pessoaId: number, nome: string, sobrenome: string, idade: number, profissao: string) {  // Define um construtor para a classe `Pessoa`, que é chamado ao criar uma nova instância da classe.
 
-    // Declara uma propriedade `profissao` do tipo `string`, que armazena a profissão da pessoa.
-    profissao: string;
-
-    // Define um construtor para a classe `Pessoa`, que é chamado ao criar uma nova instância da classe.
-    constructor(pessoaId: number, nome: string, sobrenome: string, idade: number, profissao: string) { 
-        // Inicializa a propriedade `pessoaId` com o valor fornecido no argumento do construtor.
-        this.pessoaId = pessoaId; 
-
-        // Inicializa a propriedade `nome` com o valor fornecido no argumento do construtor.
-        this.nome = nome; 
-
-        // Inicializa a propriedade `sobrenome` com o valor fornecido no argumento do construtor.
-        this.sobrenome = sobrenome; 
-
-        // Inicializa a propriedade `idade` com o valor fornecido no argumento do construtor.
-        this.idade = idade; 
-
-        // Inicializa a propriedade `profissao` com o valor fornecido no argumento do construtor.
-        this.profissao = profissao; 
+        this.pessoaId = pessoaId; // Inicializa a propriedade `pessoaId` com o valor fornecido no argumento do construtor.       
+        this.nome = nome; // Inicializa a propriedade `nome` com o valor fornecido no argumento do construtor.       
+        this.sobrenome = sobrenome; // Inicializa a propriedade `sobrenome` com o valor fornecido no argumento do construtor.        
+        this.idade = idade; // Inicializa a propriedade `idade` com o valor fornecido no argumento do construtor.       
+        this.profissao = profissao; // Inicializa a propriedade `profissao` com o valor fornecido no argumento do construtor.
     }
 }
+ 
--- src/app/components/pessoas/pessoas.component.ts
+++ src/app/components/pessoas/pessoas.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common'; 
-import { HttpClientModule } from '@angular/common/http';  
+import { FormControl, FormGroup } from '@angular/forms';
 import { PessoasService } from '../../pessoas.service';
+import { Pessoa } from '../../Pessoa';
+
+
 
 @Component({
-  selector: 'app-pessoas',  
+  selector: 'app-pessoas',
   templateUrl: './pessoas.component.html',
   styleUrls: ['./pessoas.component.css']
 })
 export class PessoasComponent implements OnInit {
   formulario: any;
   tituloFormulario: string = "";
+  pessoas: Pessoa[] = [];
+
+  visibilidadeTabela: boolean = true;
+  visibilidadeFormulario: boolean = false;
 
   constructor(private pessoasService: PessoasService) { }
 
   ngOnInit(): void {
+
+    this.pessoasService.PegarTodos().subscribe(resultado => {
+      this.pessoas = resultado;
+    });
+
+
+  }
+
+  ExibirFormularioCadastro(): void {
+    this.visibilidadeTabela = false;
+    this.visibilidadeFormulario = true;
     this.tituloFormulario = "Nova Pessoa";
     this.formulario = new FormGroup({
       nome: new FormControl(null),
@@ -25,12 +41,49 @@ export class PessoasComponent implements OnInit {
     });
   }
 
+  ExibirFormularioCadastroAtualizacao(pessoaId: number): void {
+    this.visibilidadeTabela = false;
+    this.visibilidadeFormulario = true;
+
+    this.pessoasService.PegarPeloId(pessoaId).subscribe(resultado => {
+      this.tituloFormulario = `Atualizar ${resultado.nome} ${resultado.sobrenome}`;
+      this.formulario = new FormGroup({
+        pessoaId: new FormControl(resultado.pessoaId),
+        nome: new FormControl(resultado.nome),
+        sobrenome: new FormControl(resultado.sobrenome),
+        idade: new FormControl(resultado.idade),
+        profissao: new FormControl(resultado.profissao)
+      });
+    });
+  }
+
   EnviarFormulario(): void {
     const pessoa = this.formulario.value;
 
-    this.pessoasService.SalvarPessoa(pessoa).subscribe(resultado => {
-      alert('Pessoa inserida com sucesso');
-    });
+    if (pessoa.pessoaId > 0) {
+      this.pessoasService.AtualizarPessoa(pessoa).subscribe(resultado => {
+        this.visibilidadeFormulario = false;
+        this.visibilidadeTabela = true;
+        alert('Pessoa atualizada com sucesso');
+        this.pessoasService.PegarTodos().subscribe(registros => {
+          this.pessoas = registros;
+        });
+      });
+    }
+    else {
+      this.pessoasService.SalvarPessoa(pessoa).subscribe(resultado => {
+        this.visibilidadeFormulario = false;
+        this.visibilidadeTabela = true;
+        alert('Pessoa inserida com sucesso');
+        this.pessoasService.PegarTodos().subscribe(registros => {
+          this.pessoas = registros;
+        });
+      });
+    }
   }
-  
+  Voltar(): void {
+    this.visibilidadeTabela = true;
+    this.visibilidadeFormulario = false;
+  }
+
 }
